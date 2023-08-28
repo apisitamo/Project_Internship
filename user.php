0c443ed53cedf7792f7026b7795db6ff5ac53d20
aurbrlrza <aurbrlrza@gmail.com>
2023-08-28 09:34:34 +0700

    Update user.php

--- user.php
+++ user.php
@@ -1,13 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
 <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
 <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
-<?php include 'include/head.php';
+<?php
+include 'include/head.php';
 include('server.php');
 ?>
 <style>
     .container {
         display: flex;
         flex-direction: column;
-        height: 100vh;
+        height: 100%;
     }
 
     .container-top {
@@ -60,81 +63,84 @@ include('server.php');
 </style>
 
 <body>
-    <div class="homeheader">
-        <h2 style="text-align: center;">User detail</h2>
-    </div>
-    <div class="container">
-        <div class="container-top">
-            <div class="left-box">
-                <div class="homecontent">
-                    <?php if (isset($_SESSION['username'])) : ?>
-                        <p style="text-align: center;">welcome <strong>
-                                <?php echo $_SESSION['username'] ?>
-                            </strong></p>
-                    <?php endif ?>
+    <section>
+        <div class="homeheader">
+            <h2 style="text-align: center;">User detail</h2>
+        </div>
+        <div class="container">
+            <div class="container-top">
+                <div class="left-box">
+                    <div class="homecontent">
+                        <?php if (isset($_SESSION['username'])) : ?>
+                            <p style="text-align: center;">welcome <strong>
+                                    <?php echo $_SESSION['username'] ?>
+                                </strong></p>
+                        <?php endif ?>
+                    </div>
+                    <div class="homecontent">
+                        <?php if (isset($_SESSION['username'])) : ?>
+                            <p style="text-align: center;">welcome <strong>
+                                    <?php echo $_SESSION['username'] ?>
+                                </strong></p>
+                        <?php endif ?>
+                    </div>
+                    <div class="input-group">
+                        <label for="username">เบอร์โทร</label>
+                        <input tel="number" name="username">
+                    </div>
+                    <div class="input-group">
+                        <label for="username">เบอร์โทร</label>
+                        <input tel="number" name="username">
+                    </div>
                 </div>
-                <div class="homecontent">
-                    <?php if (isset($_SESSION['username'])) : ?>
-                        <p style="text-align: center;">welcome <strong>
-                                <?php echo $_SESSION['username'] ?>
-                            </strong></p>
-                    <?php endif ?>
-                </div>
-                <div class="input-group">
-                    <label for="username">เบอร์โทร</label>
-                    <input tel="number" name="username">
-                </div>
-                <div class="input-group">
-                    <label for="username">เบอร์โทร</label>
-                    <input tel="number" name="username">
+                <div class="right-box">
+                    <div class="input-group">
+                        <label for="username">เบอร์โทร</label>
+                        <input tel="number" name="username">
+                    </div>
                 </div>
             </div>
-            <div class="right-box">
-                <div class="input-group">
-                    <label for="username">เบอร์โทร</label>
-                    <input tel="number" name="username">
-                </div>
+            <div class="bottom-box">
+                <table>
+                    <tr>
+                        <th>ลำดับ</th>
+                        <th>ชนิด</th>
+                        <th>รายการ</th>
+                        <th>จำนวน</th>
+                        <th>ราคา</th>
+                        <th>สถานะ</th>
+                    </tr>
+
+                    <?php
+                    $db = mysqli_connect($servername, $username, $password, $dbname);
+
+                    // ดึงข้อมูลจากฐานข้อมูล
+                    $query = "SELECT * FROM user ORDER BY id DESC";
+                    $result = mysqli_query($db, $query);
+
+                    $order = 1;
+                    while ($row = mysqli_fetch_assoc($result)) {
+                        echo '<tr>';
+                        echo '<td>' . $order . '</td>';
+                        echo '<td>' . $row['type'] . '</td>';
+                        echo '<td>' . $row['item'] . '</td>';
+                        echo '<td>' . $row['quantity'] . '</td>';
+                        echo '<td>' . $row['price'] . '</td>';
+                        echo '<td>' . $row['status'] . '</td>';
+                        echo '</tr>';
+
+                        $order++;
+                    }
+                    ?>
+
+                </table>
             </div>
         </div>
-        <div class="bottom-box">
-            <table>
-                <tr>
-                    <th>ลำดับ</th>
-                    <th>ชนิด</th>
-                    <th>รายการ</th>
-                    <th>จำนวน</th>
-                    <th>ราคา</th>
-                    <th>สถานะ</th>
-                </tr>
-
-                <?php
-                $db = mysqli_connect($servername,$username,$password,$dbname);
-
-                // ดึงข้อมูลจากฐานข้อมูล
-                $query = "SELECT * FROM user ORDER BY id DESC";
-                $result = mysqli_query($db, $query);
-
-                $order = 1;
-                while ($row = mysqli_fetch_assoc($result)) {
-                    echo '<tr>';
-                    echo '<td>' . $order . '</td>';
-                    echo '<td>' . $row['type'] . '</td>';
-                    echo '<td>' . $row['item'] . '</td>';
-                    echo '<td>' . $row['quantity'] . '</td>';
-                    echo '<td>' . $row['price'] . '</td>';
-                    echo '<td>' . $row['status'] . '</td>';
-                    echo '</tr>';
-
-                    $order++;
-                }
-                ?>
-
-            </table>
-        </div>
-    </div>
-
-
-
-    
+    </section>
+    <?php include 'include/footer.php'; ?>
 </body>
-<?php include 'include/footer.php'; ?>
+<script>
+    AOS.init();
+</script>
+
+</html>
