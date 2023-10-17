3a623e19c8c914dfcd68cedb50264fa05d87cb44
aurbrlrza <aurbrlrza@gmail.com>
2023-10-17 14:31:54 +0700

    Update allcoursedetail.php

--- allcoursedetail.php
+++ allcoursedetail.php
@@ -563,8 +563,7 @@
                                 <?= $ordernow ?>
                             </p>
                         </button>
-                        <button class="calen" style="margin-top: 5px;background-color:#FBDFCF;color:#5F6368;"
-                            data-aos="fade-up" data-aos-duration="2000">
+                        <button class="calen" style="margin-top: 5px;background-color:#FBDFCF;color:#5F6368;" data-aos="fade-up" data-aos-duration="2000">
                             <?= $free ?>
                         </button>
                     </div>
@@ -584,15 +583,12 @@
                 </div>
                 <div class="col-lg-6">
                     <div class="wrap-contact">
-                        <div class="item call" data-aos="fade-up" data-aos-duration="2000"><i
-                                class="fa-regular fa-phone"></i><span>086-322-1922</span></div>
+                        <div class="item call" data-aos="fade-up" data-aos-duration="2000"><i class="fa-regular fa-phone"></i><span>086-322-1922</span></div>
                         <a href="https://line.me/ti/p/~@108toots">
-                            <div class="item line" data-aos="fade-up" data-aos-duration="2000"><img class="line-img"
-                                    src="assets/images/line.png" alt=""><span>@bsathailand</span></div>
+                            <div class="item line" data-aos="fade-up" data-aos-duration="2000"><img class="line-img" src="assets/images/line.png" alt=""><span>@bsathailand</span></div>
                         </a>
                         <a href="https://th-th.facebook.com/BSABangkok/">
-                            <div class="item facebook" data-aos="fade-up" data-aos-duration="2000"><i
-                                    class="bi bi-facebook"></i><span>Bangkok Spa Academy</span></div>
+                            <div class="item facebook" data-aos="fade-up" data-aos-duration="2000"><i class="bi bi-facebook"></i><span>Bangkok Spa Academy</span></div>
                         </a>
                     </div>
                 </div>
@@ -611,7 +607,7 @@
             <div class="popup-content">
                 <span class="close-popup" id="close-popup1">&times;</span>
                 <div class="homecontent">
-                    <?php if (isset($_SESSION['username'])): ?>
+                    <?php if (isset($_SESSION['username'])) : ?>
                         <div class="box">
                             <div class="container">
                                 <div class="row">
@@ -627,7 +623,7 @@
                                 </button>
                             </div>
                         </div>
-                    <?php else: ?>
+                    <?php else : ?>
                         <div class="box">
                             <div class="container">
                                 <div class="row">
@@ -723,7 +719,7 @@
                                     <?= $slip ?>
                                 </h5>
                             </div>
-                            <?php
+                        <?php
                         } else { ?>
                             <div class="homecontent">
                                 <div class="container">
@@ -739,7 +735,7 @@
                                     </a>
                                 </div>
                             </div>
-                        <?php }
+                <?php }
                     } else {
                         // ไม่พบข้อมูลของ username นี้
                         echo "No information found for this username.";
@@ -769,7 +765,7 @@
                     <?= $dayss ?>
                 </p>
                 <div class="right-calinput">
-                    <?php for ($i = 1; $i <= $day; $i++): ?>
+                    <?php for ($i = 1; $i <= $day; $i++) : ?>
                         <input type="date" class="form-control" name="dd" required>
                     <?php endfor ?>
                 </div>
@@ -821,8 +817,11 @@
 
         <div class="popup" id="popup5">
             <div class="popup-content">
-                <span class="close-popup" id="close-popup5">&times;</span>
+                <!-- <span class="close-popup" id="close-popup5">&times;</span> -->
                 <div id="calendar1"></div>
+                <button class="button-close" id="button-close5">
+                    <?= $cancle ?>
+                </button>
             </div>
         </div>
 
@@ -865,6 +864,7 @@
 
     const calen = document.querySelectorAll('.calen');
     const popup5 = document.querySelector('#popup5');
+    const buttonclosecalen = document.querySelector('#button-close5');
     const closecalenpopup = document.querySelector('#close-popup5');
 
     openpopup.forEach(button => {
@@ -953,8 +953,14 @@
             clickOverlay1.style.display = 'block';
         });
     });
-    closecalenpopup.addEventListener('click', () => {
-        console.log("X calen popup ");
+    // closecalenpopup.addEventListener('click', () => {
+    //     console.log("X calen popup ");
+    //     popup5.style.display = 'none';
+    //     clickOverlay1.style.display = 'none';
+    //     // location.reload();
+    // });
+    buttonclosecalen.addEventListener('click', () => {
+        console.log("close BTN calen POPUP");
         popup5.style.display = 'none';
         clickOverlay1.style.display = 'none';
         // location.reload();
@@ -962,7 +968,7 @@
 </script>
 
 <script>
-    document.getElementById('backButton').addEventListener('click', function () {
+    document.getElementById('backButton').addEventListener('click', function() {
         window.history.back();
     });
 </script>
@@ -1076,9 +1082,9 @@
 
 
 <script>
-    $(document).ready(function () {
+    $(document).ready(function() {
 
-        $(".SBdate").click(function () {
+        $(".SBdate").click(function() {
 
             $(".alert1").css("display", "none");
             $(".alert2").css("display", "none");
@@ -1087,7 +1093,7 @@
             $(".alert5").css("display", "none");
 
             var dates = [];
-            $("input[name='dd']").each(function () {
+            $("input[name='dd']").each(function() {
                 dates.push($(this).val());
             });
 
@@ -1102,9 +1108,9 @@
             }
 
             // ตรวจสอบว่าทุก input type="date" มีค่าเป็นวันที่เดียวกันและซ้ำกันแม้แต่วันเดียวกัน
-            if (dates.every(function (date) {
-                return date === dates[0];
-            })) {
+            if (dates.every(function(date) {
+                    return date === dates[0];
+                })) {
                 $(".alert1").css("display", "none");
                 $(".alert2").css("display", "none");
                 $(".alert3").css("display", "block"); // เปิด alert3 ถ้ามีวันที่เดียวกัน
@@ -1124,7 +1130,7 @@
                 }
             }
 
-            var hasDuplicateDates = Object.values(dateCounts).some(function (count) {
+            var hasDuplicateDates = Object.values(dateCounts).some(function(count) {
                 return count >= 2;
             });
 
@@ -1139,14 +1145,14 @@
 
             // ตรวจสอบว่าวันที่ที่ผู้ใช้เลือกหลังวันปัจจุบันหรือไม่
             var today = new Date();
-            var selectedDates = dates.map(function (date) {
+            var selectedDates = dates.map(function(date) {
                 return new Date(date);
             });
 
             // ตรวจสอบว่ามีวันเสาร์หรืออาทิตย์ในรายการวันที่ที่ผู้ใช้เลือก
-            if (selectedDates.some(function (selectedDate) {
-                return selectedDate.getDay() === 0 || selectedDate.getDay() === 6; // 0 คือวันอาทิตย์ และ 6 คือวันเสาร์
-            })) {
+            if (selectedDates.some(function(selectedDate) {
+                    return selectedDate.getDay() === 0 || selectedDate.getDay() === 6; // 0 คือวันอาทิตย์ และ 6 คือวันเสาร์
+                })) {
                 $(".alert1").css("display", "none");
                 $(".alert2").css("display", "none");
                 $(".alert3").css("display", "none");
@@ -1155,9 +1161,9 @@
                 return; // ไม่ดำเนินการต่อ
             }
 
-            if (selectedDates.some(function (selectedDate) {
-                return selectedDate < today;
-            })) {
+            if (selectedDates.some(function(selectedDate) {
+                    return selectedDate < today;
+                })) {
                 $(".alert1").css("display", "none");
                 $(".alert2").css("display", "none");
                 $(".alert3").css("display", "none");
@@ -1174,7 +1180,7 @@
                 data: {
                     dates: dates
                 },
-                success: function (response) {
+                success: function(response) {
                     if (response == "duplicate") {
                         $(".alert1").css("display", "none");
                         $(".alert2").css("display", "block");
@@ -1191,7 +1197,7 @@
                                 dates: dates,
                                 name: name
                             },
-                            success: function (response) {
+                            success: function(response) {
                                 // กระทำหลังจากสำเร็จ
                                 $("#popup3").css("display", "none");
                                 $("#popup4").css("display", "flex");
@@ -1222,19 +1228,19 @@
                                     data: formData,
                                     contentType: false,
                                     processData: false,
-                                    success: function (response) {
+                                    success: function(response) {
                                         // alert(response);
                                     }
                                 });
                             },
-                            error: function (xhr, status, error) {
+                            error: function(xhr, status, error) {
                                 // กระทำหลังจากเกิดข้อผิดพลาด
                                 alert("เกิดข้อผิดพลาดในการบันทึกข้อมูล");
                             }
                         });
                     }
                 },
-                error: function (xhr, status, error) {
+                error: function(xhr, status, error) {
                     // กระทำหลังจากเกิดข้อผิดพลาดในการตรวจสอบวันที่
                     alert("เกิดข้อผิดพลาดในการตรวจสอบวันที่");
                 }
